6e8d836d93ea5962e9192ba9c6feb5fa90c6dc5b
Pankaj Kumar Jha <pankaj.kumar.jha@accenture.com>
2022-06-10 12:13:23 +0800

    refactor

--- lib/src/utility/input_validator.dart
+++ lib/src/utility/input_validator.dart
@@ -4,9 +4,9 @@ String regexSource = r'^[0-9]+$';
 String emailRegex =
     r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
 
-class InputFormatterValidator implements TextInputFormatter {
-  final AmountValidator validator;
-  InputFormatterValidator({required this.validator});
+class TXLInputFormatterValidator implements TextInputFormatter {
+  final TXLAmountValidator validator;
+  TXLInputFormatterValidator({required this.validator});
   TextEditingValue formatEditUpdate(
       TextEditingValue oldValue, TextEditingValue newValue) {
     var isOldValueValid = validator.isValid(oldValue.text);
@@ -19,13 +19,13 @@ class InputFormatterValidator implements TextInputFormatter {
   }
 }
 
-abstract class AmountValidator {
+abstract class TXLAmountValidator {
   bool isValid(String value);
 }
 
-class AmountRegexValidator extends AmountValidator {
+class TXLAmountRegexValidator extends TXLAmountValidator {
   final String source;
-  AmountRegexValidator({required this.source});
+  TXLAmountRegexValidator({required this.source});
   bool isValid(String value) {
     try {
       var regex = RegExp(source);
@@ -41,9 +41,9 @@ class AmountRegexValidator extends AmountValidator {
   }
 }
 
-class EmailRegexValidator extends AmountValidator {
+class TXLEmailRegexValidator extends TXLAmountValidator {
   final String source;
-  EmailRegexValidator({required this.source});
+  TXLEmailRegexValidator({required this.source});
   @override
   bool isValid(String value) {
     try {

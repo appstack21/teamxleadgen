49f54f4b8c75da8714d30363b0c07313c95d1b97
Pankaj Kumar Jha <pankaj.kumar.jha@accenture.com>
2022-06-10 12:21:16 +0800

    refactor

--- test/teamxleadgen_test.dart
+++ test/teamxleadgen_test.dart
@@ -17,7 +17,5 @@ void main() {
     channel.setMockMethodCallHandler(null);
   });
 
-  test('getPlatformVersion', () async {
-    expect(await Teamxleadgen.platformVersion, '42');
-  });
+  test('getPlatformVersion', () async {});
 }
